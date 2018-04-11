6e28b62e54b3d5b552957753571079516e0e8671
MikaHeinemann <161wit08@o365.htl-leoben.at>
2018-04-11 14:14:06 +0200

    Level1 Final Funktion

--- Level1.java
+++ Level1.java
@@ -4,7 +4,6 @@ public class Level1 {
 	public static void main(String[] args) {
 		int betrag = Integer.parseInt(args[0]);
 		int anzahlMuenzen = Integer.parseInt(args[1]);
-		int[] muenzen = new int[anzahlMuenzen];
 		int muenzenges = 0;
 		for(int m = 0; m<anzahlMuenzen;m++) {
 			int offset = 2;
